9e90b47a6e50b5708c9dbcc120939a7b67f98686
Cassie Musolf and Jiwon Kang <cassie+jiwon@>
2017-02-22 12:00:11 -0800

    add and run test for Save() method

--- Tests/CuisineTest.cs
+++ Tests/CuisineTest.cs
@@ -6,38 +6,53 @@ using System.Data.SqlClient;
 
 namespace BestRestaurants
 {
-  public class CuisineTest : IDisposable
-  {
-    public CuisineTest()
+    public class CuisineTest : IDisposable
     {
-      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=bestrestaurants_test;Integrated Security=SSPI;";
+        public CuisineTest()
+        {
+            DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=bestrestaurants_test;Integrated Security=SSPI;";
+        }
+
+        [Fact]
+        public void Test_CuisineEmptyAtFirst()
+        {
+            //Arrange, Act
+            int result = Cuisine.GetAll().Count;
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test_Equal_ReturnsTrueForSameName()
+        {
+            //Arrange, Act
+            Cuisine firstCuisine = new Cuisine("Mexican");
+            Cuisine secondCuisine = new Cuisine("Mexican");
+
+            //Assert
+            Assert.Equal(firstCuisine, secondCuisine);
+        }
+
+        [Fact]
+        public void Test_Save_SavesCuisineToDatabase()
+        {
+            //Arrange
+            Cuisine testCuisine = new Cuisine("French");
+            testCuisine.Save();
+
+            //Act
+            List<Cuisine> result = Cuisine.GetAll();
+            List<Cuisine> testList = new List<Cuisine>{testCuisine};
+
+            //Assert
+            Assert.Equal(testList, result);
+        }
+
+        public void Dispose()
+        {
+            Cuisine.DeleteAll();
+        }
+
     }
-
-    [Fact]
-    public void Test_CuisineEmptyAtFirst()
-    {
-        //Arrange, Act
-        int result = Cuisine.GetAll().Count;
-
-        //Assert
-        Assert.Equal(0, result);
-    }
-
-    [Fact]
-    public void Test_Equal_ReturnsTrueForSameName()
-    {
-        //Arrange, Act
-        Cuisine firstCuisine = new Cuisine("Mexican");
-        Cuisine secondCuisine = new Cuisine("Mexican");
-
-        //Assert
-        Assert.Equal(firstCuisine, secondCuisine);
-    }
-
-    public void Dispose()
-    {
-      Cuisine.DeleteAll();
-    }
-
-  }
 }
