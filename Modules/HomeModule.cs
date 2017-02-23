0fdb7acde6d2be51106275167d8e203efbb190ca
Cassie Musolf and Jiwon Kang <cassie+jiwon@>
2017-02-22 16:30:35 -0800

    add cuisines and restrants cshtml pages

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -15,11 +15,47 @@ namespace BestRestaurants
             Get["/restaurants"] = _ => {
                 List<Restaurant> AllRestaurants = Restaurant.GetAll();
                 return View["restaurants.cshtml", AllRestaurants];
-              };
+            };
             Get["/cuisines"] = _ => {
                 List<Cuisine> AllCuisines = Cuisine.GetAll();
                 return View["cuisines.cshtml", AllCuisines];
             };
+            Get["/cuisines/new"] = _ => {
+                return View["cuisines_form.cshtml"];
+            };
+            Post["/cuisines/new"] = _ => {
+                Cuisine newCuisine = new Cuisine(Request.Form["cuisine-name"]);
+                newCuisine.Save();
+                return View["success.cshtml"];
+            };
+            Get["/restaurants/new"] = _ => {
+                List<Cuisine> AllCuisines = Cuisine.GetAll();
+                return View["restaurants_form.cshtml", AllCuisines];
+            };
+            Post["/restaurants/new"] = _ => {
+                Restaurant newRestaurant = new Restaurant(Request.Form["restaurant-name"], Request.Form["restaurant-location"], Request.Form["restaurant-price"], Request.Form["cuisine-id"]);
+                newRestaurant.Save();
+                return View["success.cshtml"];
+            };
+            Post["/restaurants/delete"] = _ => {
+                Restaurant.DeleteAll();
+                return View["cleared.cshtml"];
+            };
+            Get["/cuisines/{id}"] = parameters => {
+                Dictionary<string, object> model = new Dictionary<string, object>();
+                var SelectedCuisine = Cuisine.Find(parameters.id);
+                var CuisineRestaurants = SelectedCuisine.GetRestaurants();
+                model.Add("cuisine", SelectedCuisine);
+                model.Add("restaurants", CuisineRestaurants);
+                return View["cuisine.cshtml", model];
+            };
+            // Get["/restaurants/{id}"] = parameters => {
+            //     Dictionary<string, object> model = new Dictionary<string, object>();
+            //     var SelectedRestaurant = Restaurant.Find(parameters.id);
+            //     model.Add("restaurant", SelectedRestaurant);
+            //     // model.Add("restaurants", RestaurantCuisines);
+            //     return View["restaurant.cshtml", model];
+            // };
         }
     }
 }
